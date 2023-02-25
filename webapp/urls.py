0b1e77fa45bb6fd8acb46cb60f71615c739a9f03
Valentin Kim <valentink@yandex.ru>
2023-02-25 15:09:16 +0600

    Реализовано добавление и редактирование записей, добавлены формы и шаблоны, валидация полей.

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from webapp.views.records_view import records_view
+from webapp.views.records_view import records_view, record_add, record_edit
 
 
 urlpatterns = [
     path("", records_view, name='index'),
-    path("guestbook/", records_view)
-
+    path("guestbook/", records_view),
+    path('guestbook/add', record_add, name='record_add'),
+    path('guestbook/<int:pk>/edit', record_edit, name='record_edit')
 ]
+
--- webapp/views/records_view.py
+++ webapp/views/records_view.py
@@ -1,5 +1,7 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from webapp.forms import GuestbookForm
 from webapp.models import Guestbook
 
 
@@ -9,3 +11,45 @@ def records_view(request: WSGIRequest):
         'records': records
     }
     return render(request, 'records.html', context=context)
+
+
+# def delete_product(request, pk):
+#     product = get_object_or_404(Products, pk=pk)
+#     return render(request, 'product_confirm_delete.html', context={'product': product})
+
+
+# def confirm_delete(request, pk):
+#     product = get_object_or_404(Products, pk=pk)
+#     product.delete()
+#     return redirect('products')
+
+def record_add(request: WSGIRequest):
+    if request.method == "GET":
+        form = GuestbookForm()
+        return render(request, 'record_add.html',
+                      context={
+                          'form': form
+                      })
+
+    form = GuestbookForm(data=request.POST)
+    if not form.is_valid():
+        return render(request, 'record_add.html',
+                      context={
+                          'form': form
+                      })
+    else:
+        Guestbook.objects.create(**form.cleaned_data)
+        return redirect('index')
+
+
+def record_edit(request, pk):
+    record = get_object_or_404(Guestbook, pk=pk)
+
+    if request.method == "POST":
+        form = GuestbookForm(request.POST, instance=record)
+        if form.is_valid():
+            record.save()
+            return redirect('index')
+
+    form = GuestbookForm(instance=record)
+    return render(request, 'record_edit_view.html', context={'form': form,'record': record})
